2b956feb841ff7ccab8dfa6bb9525bc60141b636
grzesk075 <grzesk075@gmail.com>
2018-09-09 17:36:34 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+var http = require("http");
+
+var port = 8081;
+
+function doGet(req, rsp) {
+
+    rsp.writeHead(200, {'Content-Type': 'text/plain'});
+    rsp.end('Hello world.\n');
+}
+
+http.createServer(doGet).listen(port);
+
+console.log("HTTP server is listening on port " + port);
