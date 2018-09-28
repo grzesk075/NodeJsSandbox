b1563dedcd4a6322b0ed1cc3667688c323fd62a0
Grzegorz Kuprianowicz <grzegorz.kuprianowicz@tomtom.com>
2018-09-28 14:32:29 +0200

    Add events to callbacks.js.

--- tutorials/callbacks.js
+++ tutorials/callbacks.js
@@ -1,9 +1,21 @@
 /**
+ * Event and callback example.
+ * 
  * Run in main project folder using a command as follows:
  * node tutorials/callbacks.js
  */
 
 var fs = require("fs");
+var events = require("events");
+
+var eventsEmitter = new events.EventEmitter();
+
+var onDoneHandler = function(msg) {
+
+    console.log('OnDoneHandler ' + msg);
+}
+
+eventsEmitter.on('done', onDoneHandler);
 
 /**
  * Blocking function (executes in sequence).
@@ -12,6 +24,8 @@ var syncRead = function() {
 
     var buff = fs.readFileSync('./tutorials/intake.txt');
     console.log(buff.toString());
+
+    eventsEmitter.emit('done', 'sync');
 }
 
 /**
@@ -23,6 +37,8 @@ var callback = function(err, buff) {
         return console.error(err);
     
     console.log(buff.toString());
+
+    eventsEmitter.emit('done', 'async');
 }
 
 syncRead();
