5bb08442e8269ca5ae7c01c880f7315fea45e270
Grzegorz Kuprianowicz <grzesk075@gmail.com>
2018-09-29 12:01:44 +0200

    Add gzip functionality to stream tutorial.

--- tutorials/streams.js
+++ tutorials/streams.js
@@ -1,8 +1,12 @@
 /**
- * Streams concept tutorial.
+ * Stream concept tutorial.
  */
 
  var fs = require('fs');
+ var zlib = require('zlib');
+
+ const INTAKE = './tutorials/intake.txt';
+ const OUTLET = './tutorials/intake.txt.gz';
 
  var data = '';
 
@@ -21,10 +25,12 @@
     console.log(data);
  }
 
-var readStream = fs.createReadStream('./tutorials/intake.txt');
+var readStream = fs.createReadStream(INTAKE);
 
 readStream.on('data', onData);
 readStream.on('error', onError);
 readStream.on('end', onEnd);
 
-console.log('End of main thread.');
+fs.createReadStream(INTAKE).pipe(zlib.createGzip()).pipe(fs.createWriteStream(OUTLET));
+
+console.log(INTAKE + ' compressed to ' + OUTLET);
