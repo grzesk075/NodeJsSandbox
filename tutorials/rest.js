017591ba3029cf8540e1b5683d9377248d192743
Grzegorz Kuprianowicz <grzesk075@gmail.com>
2018-09-30 16:52:38 +0200

    Populate rest tutorial.

--- tutorials/rest.js
+++ tutorials/rest.js
@@ -1,14 +1,47 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var fs = require('fs');
 
 var app = express();
+app.use(bodyParser.json());
+
+var usersBuff = fs.readFileSync('./tutorials/users.json', 'utf8');
+var usersJSON = JSON.parse(usersBuff);
 
 app.get('/users', function(req, rsp) {
 
-    rsp.end('Hello world');
+    rsp.json(usersJSON);
 });
 
-var server = app.listen(8080, () => {
+app.post('/adduser', function(req, rsp) {
+
+    var newUser = req.body;
+
+    usersJSON.users.push(newUser);
+
+    rsp.end();
+});
+
+/**
+ * This doesn't work properly because of bad design of users.json.
+ * Array is not suitable. Use properties named user<id> instead.
+ */
+app.delete('/removeuser/:id', function(req, rsp) {
+
+    var id = req.params.id;
+
+    var i = 0;
+    usersJSON.users.forEach((user) => {
+        if(user.id == id)
+            delete usersJSON.users[i];
+        
+        i++;
+    });
+
+    rsp.end();
+});
+
+var server = app.listen(8080, 'localhost', () => {
 
     var address = server.address();
     console.log('REST server is listening at http://%s:%s', address.address, address.port);
