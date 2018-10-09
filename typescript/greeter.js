2f13072e89c059632576fcbdb2eefb3231529143
Grzegorz Kuprianowicz <grzesk075@gmail.com>
2018-10-09 15:29:42 +0200

    Add type examples.

--- typescript/greeter.js
+++ typescript/greeter.js
@@ -11,3 +11,25 @@ function greeter(person) {
 }
 var greetedUser = new Student('Grażyna', 'Kowalska');
 document.body.innerHTML = greeter(greetedUser);
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 8] = "Blue";
+})(Color || (Color = {}));
+var c = Color.Red;
+var notSure = 'could be string';
+var len = notSure.length; // Assertion is like cast in other languages.
+len = notSure.length; // Another syntax for type assertion.
+var typeUnion; // In --strictNullChecks mode, null also can be assigned.
+// Function returning never must have unreachable end point.
+function error(message) {
+    throw new Error(message);
+}
+function onError(err) {
+    console.error(err);
+}
+var numbers = [1, 2, 3, 4];
+console.log(numbers);
+var tuple = ['Ala', true];
+console.log(tuple);
--- typescript/greeter.ts
+++ typescript/greeter.ts
@@ -9,10 +9,34 @@ class Student {
     // It also implicitly fits to Person interface.
 }
 
-function greeter(person: Person) {
+function greeter(person: Person): string {
     return 'Hello ' + person.firstName + ' ' + person.lastName + '.';
 }
 
-let greetedUser = new Student('Grażyna', 'Kowalska');
+let greetedUser: Person = new Student('Grażyna', 'Kowalska');
 
 document.body.innerHTML = greeter(greetedUser);
+
+
+
+enum Color {Red, Green, Blue = 8}
+let c: Color = Color.Red;
+let notSure: any = 'could be string';
+let len: number = (notSure as string).length; // Assertion is like cast in other languages.
+len = (<string>notSure).length; // Another syntax for type assertion.
+let typeUnion: string | number | null; // In --strictNullChecks mode, null also can be assigned.
+
+// Function returning never must have unreachable end point.
+function error(message: string): never {
+    throw new Error(message);
+}
+
+function onError(err: Error): void {
+    console.error(err);
+}
+
+let numbers: Array<number> = [1,2,3,4];
+console.log(numbers);
+
+let tuple: [string, boolean] = ['Ala', true];
+console.log(tuple);
